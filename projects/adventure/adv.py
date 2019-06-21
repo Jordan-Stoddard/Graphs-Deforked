49ae5a3df64a965dcdcd42cdd15c08afb0449136
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-21 09:06:42 -0700

    room_graph 2 passed

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -25,12 +25,13 @@ exits = player.currentRoom.getExits()
 exit_graph = {}
 # Set starting room in graph.
 exit_graph[room_id] = {i : '?' for i in exits}
-
+unexplored = None
 
 
 looping = True
 while looping:
     current_room = exit_graph[room_id]
+    temp_room = exit_graph[room_id]
     for direction in current_room:
         if current_room[direction] == '?':
             player.travel(direction)
@@ -53,8 +54,46 @@ while looping:
                 exit_graph[player.currentRoom.id]['e'] = room_id
                 room_id = player.currentRoom.id
                 break
-        else:
-        
+        if '?' not in current_room.values():
+            for room in exit_graph:
+                if '?' in exit_graph[room].values():
+                    unexplored = True
+
+            if unexplored == True:
+                visited = set()
+                q = Queue()
+                q.enqueue([player.currentRoom.id])
+
+                while q.size() > 0:
+                    if '?' in temp_room.values():
+                        break
+
+                    path = q.dequeue()
+                    v = path[-1]
+
+                    if v not in visited:
+                        visited.add(v)
+
+                        for neighbor in exit_graph[v]:
+                            if temp_room[neighbor] not in visited:
+                                path_copy = path.copy()
+                                path_copy.append(temp_room[neighbor])
+                                q.enqueue(path_copy)
+                                temp_room = exit_graph[temp_room[neighbor]]
+                unexplored = False
+                path = q.dequeue()
+                for room in path:
+                    if room == player.currentRoom.id:
+                        pass
+                    else:
+                        if room in current_room.values():
+                            direction = list(current_room.keys())[list(current_room.values()).index(room)]
+                            player.travel(direction)
+                            traversalPath.append(f'{direction}')
+                            room_id = player.currentRoom.id
+                            current_room = exit_graph[room_id]
+            else:
+                looping = False
 
 
 
